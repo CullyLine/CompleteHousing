09535574319c81ca373ded815eb2d5af4d5dbb55
CullyLine <Linebergercully@gmail.com>
2024-03-18 01:43:19 -0400

    Removed old commented out code that was at the bottom

--- FurnitureService.lua
+++ FurnitureService.lua
@@ -49,10 +49,6 @@ FurnitureService.userPlaceFurniture = function(player : Player, furnitureModelNa
 
 end
 
--- game.ReplicatedStorage.DevRemoteEvent.OnServerEvent:Connect(function(player, furnitureModelName, cframe)
--- 	FurnitureService.userPlaceFurniture(player, furnitureModelName, cframe)
--- end)
-
 userAttemptPlaceFurnitureRemoteEvent.OnServerEvent:Connect(function(player, furnitureArgs)
 	local furnitureModelName = furnitureArgs["FurnitureModelName"]
 
@@ -78,30 +74,6 @@ FurnitureService.userDeleteFurniture = function(player : Player, furnitureGUID :
 	-- Also, delete the object from the player's data.
 
 
-end
-
--- FurnitureService.placeFurniture = function(player : Player, furnitureArgs)
--- 	local furnitureModel = furnitureModels:FindFirstChild(furnitureArgs["FurnitureID"]):Clone()
--- 	furnitureModel.Parent = workspace
--- 	local origin = workspace.Origin.Position
--- 	furnitureModel:SetPrimaryPartCFrame(
--- 		CFrame.new(Vector3.new(origin.X + furnitureArgs["PositionOffsetX"], origin.Y + furnitureArgs["PositionOffsetY"], origin.Z + furnitureArgs["PositionOffsetZ"]))
--- 		* CFrame.Angles(math.rad(furnitureArgs["RotationOffsetX"]), math.rad(furnitureArgs["RotationOffsetY"]), math.rad(furnitureArgs["RotationOffsetZ"]))
--- 	)
--- 	furnitureModel.Primary.SurfaceGui.ImageLabel.ImageTransparency = 1
--- 	furnitureArgs["New"] = false
--- 	if (not furnitureArgs["GUID"]) then
--- 		furnitureArgs["UserID"] = player.UserId
--- 		furnitureArgs["GUID"] = game:GetService("HttpService"):GenerateGUID(false)
--- 		furnitureArgs["New"] = true
--- 		table.insert(dataService.data[player.Name]["Furniture"], furnitureArgs)
--- 	end
--- 	print(game:GetService("HttpService"):JSONEncode(dataService.data[player.Name]))
--- end
-
--- local placeFurnitureRemoteEvent = replicatedStorage:WaitForChild("PlaceFurnitureRemoteEvent", math.huge)
--- placeFurnitureRemoteEvent.OnServerEvent:Connect(function(player, furnitureArgs)
--- 	FurnitureService.placeFurniture(player, furnitureArgs)
--- end)
+end 
 
 return FurnitureService
